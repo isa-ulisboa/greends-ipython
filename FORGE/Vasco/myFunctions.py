5c0ffa2d1410e650b4613358512b479a4c3cf28f
VascoFlorentino <vasco.florentino.volf@gmail.com>
2022-10-07 15:32:17 +0100

    aula 7/10/22

---
+++ FORGE/Vasco/myFunctions.py
@@ -0,0 +1,18 @@
+def myFirstGreenDSFunction(arg1, agr2):
+    print(arg1,agr2)
+    return 'GREEN DS'
+
+def test():
+    return 'something'
+a = test()
+print (a)
+
+var1=23
+var2='ds'
+var3=26
+
+list = (var1, var2, var3)
+
+print(var1)
+print(var2)
+print(var3)
