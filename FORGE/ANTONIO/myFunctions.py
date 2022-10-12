721719661abf04319039f5a0d4b466e1973146d2
antonioclacerda <antonio17@edu.ulisboa.pt>
2022-10-12 19:12:52 +0100

    CorreçaoEx4

--- FORGE/ANTONIO/myFunctions.py
+++ FORGE/ANTONIO/myFunctions.py
@@ -1,31 +1,36 @@
+from cmath import log
+from math import e
+import math
+
+
 def myfunction():
     return "hi"
 
 def function2():
     return "ola"
 
-from cmath import log
-import numpy
 
 
 
-def FieldCapacity(C,D,S,OM,TP):
+def FieldCapacity(C,S,D,OM,TP):
     
     val1=0.7919
     val2=0.001691*C
     val3=0.29619*D
-    val4=0.000001491*S**2
-    val5=0.0000821*OM**2
-    val6=0.02427*C**-1
-    val7=0.01113*S**-1
-    val8=0.01472*numpy*log(S)
+    val4=0.000001491*(S**2)
+    val5=0.0000821*(OM**2)
+    val6=0.02427*(C**-1)
+    val7=0.01113*(S**-1)
+    val8=0.01472*(math.log(S))
     val9=0.0000733*OM*C
     val10=0.000619*D*C
     val11=0.001183*D*OM
     val12=0.0001664*TP*S
 
+
     return val1+val2-val3-val4+val5+val6+val7+val8-val9-val10-val11-val12
 
+print(FieldCapacity(0.2,0.2,1.3,0.05,1))
 
 
        #C-percentage clay;D-bulk density;S-percentage silt;OM-percentage organic matter;
