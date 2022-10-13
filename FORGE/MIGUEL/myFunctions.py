0c6e98c2369488fdee6cf1b1ac76a81a321bb514
Miguel Paulo <miguel.ventura.paulo@gmail.com>
2022-10-13 17:00:56 +0100

    Exercise 3 and 4

--- FORGE/MIGUEL/myFunctions.py
+++ FORGE/MIGUEL/myFunctions.py
@@ -3,3 +3,22 @@ def myfirst():
 
 def mysecond():
     return "Hi"
+
+#Soil Field Capacity
+
+def soil_field_capacity(C,D,S,OM,topsoil):
+    a=0.7919
+    b=0.001691*C
+    c=0.29619*D
+    e=0.0000821*OM**2
+    f=0.02427*C**-1
+    g=0.01113*S**-1
+    h=0.01472*ln(S)
+    i=0.0000733*OM*C
+    j=0.000619*D*C
+    k=0.001183*D*OM
+    l=0.0001664*topsoil*S
+
+return a+b-c-d+e+f+g+h,i-j-k-l
+
+print(soil_field_capacity(0.2,0.2,1.3,0.05,1))
