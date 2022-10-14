48f7b952374e58ff1c0ccb36d604e0173320299d
borneah <saraasenra@icloud.com>
2022-10-14 15:18:25 +0100

    versao2.22.10.14

--- FORGE/sarasenra.py
+++ FORGE/sarasenra.py
@@ -6,3 +6,12 @@ fout = open("GreenDSfile1_sample.csv","w")
 for i in range(100):
     fout.writelines(lst[1])
 fout.close()
+
+import os
+
+os.remove("GreenDSfile1_sample.csv")
+
+fp = open("GreenDSfile1.csv", 'r')
+x = len(fp.readlines())
+print('Total lines:', x)
+fp.close()
