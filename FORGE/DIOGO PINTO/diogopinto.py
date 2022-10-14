03acc7105d4402e068f210660c085f8b581258d1
DiogoPinto <diogo.praia10@gmail.com>
2022-10-14 03:37:27 +0100

    Assign#4

---
+++ FORGE/DIOGO PINTO/diogopinto.py
@@ -0,0 +1,30 @@
+#importa tudo o que está no file 
+import myFunctions
+import myForge.myOtherFunctions
+
+
+#a ir buscar a informação que esta nesse file (myfunctions), escrito pela função
+var1= myFunctions.teste2()
+print(var1)
+
+var4=myFunctions.teste3()
+print(var4)
+
+#a ir buscar a informação que esta nesse file (myotherfunctions), escrito pela função
+var2= myForge.myOtherFunctions.t3()
+print(var2)
+
+print('ola')
+
+
+
+''''
+import myFunctions
+import myForge.myOtherFunctions
+impr=myFunctions.t1()
+print(impr)
+impr2=myFunctions.t2(10,40)
+print(impr2)
+impr3=myForge.myOtherFunctions.t3()
+print (impr3)
+'''
