f5692a838c44886206e1c1291bed59168a719780
Johanna Rauberger <johannarauberger@edu.ulisboa.pt>
2022-10-16 12:01:08 +0200

    assignemt #3 and #4

--- FORGE/JOHANNA/Assignment_4.py
+++
@@ -1,5 +0,0 @@
-# Assignment 4
-# create function that returns the Volumetric Water Content at saturation (Field Capacity) a Soil
-
-# Test with clay:0.2, silt: 0.2, bulk density: 1.3, om: 0.05, topsoil: 1
-# result should be 0.606
--- FORGE/JOHANNA/myFunctions.py
+++ FORGE/JOHANNA/myFunctions.py
@@ -1,7 +1,16 @@
-# Assignment #3
+import math
 
+# Assignment #3
 # define two different functions returning two different strings
+
 def function_re_str(): 
     return("a string")
 def function_re_str2():
     return("another string")
+
+# Assignment #4
+# create function that returns the Volumetric Water Content at saturation (Field Capacity) a Soil
+
+def vol_water_content_saturated(clay,bulkdensity,silk,OM,topsoil):
+    vwcs = 0.7919 + 0.001691*clay - 0.29619*bulkdensity - 0.000001491*silk**2 + 0.0000821*OM**2 + 0.02427*(1/clay) + 0.01113/silk + 0.01472*math.log(silk) - 0.0000733*OM*clay - 0.000619*bulkdensity*clay - 0.001183*bulkdensity*OM - 0.0001664*topsoil*silk
+    return(vwcs)
