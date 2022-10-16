6ec3a5f74e2f841a283118058e820c61ff881307
VascoFlorentino <vasco.florentino.volf@gmail.com>
2022-10-16 21:32:23 +0100

    y0.5

--- FORGE/Vasco/myForge/vasco.py
+++ FORGE/Vasco/myForge/vasco.py
@@ -24,17 +24,20 @@ FORGE is the file name
 ### CASTING
 
 import random
+
 def frasealeatoria():
-    return (print('O Vasco escolheu', var1[1],'camisa', var4[1] , 'de cor',var2[0],
-     'para emprestar ao',var3[2], '.'))
+    return (print('O Vasco escolheu', var1[0],'camisas',var4[0], 'de cor',var2[0],
+     'para emprestar ao', var3[0] ,'.'))
 
-var1= [1,2,3,4]
-var2= ['azul','branca','verde','castanha']
-var3= ['Antonio','Ze','Roberto','Afonso']
-var4= {"""(s)"""}
+var1 = [2,3,4]
+var2 = ['azul','branca','verde','castanha']
+var3 = ['Antonio','Zé','Roberto','Afonso']
+var4 = ['S','L','M','XL']
 
-random.shuffle(var3)
-random.shuffle(var2)
 random.shuffle(var1)
-
+random.shuffle(var2)
+random.shuffle(var3)
+random.shuffle(var4)
 print (frasealeatoria())
+
+# print (frasealeatoria()) if var1 == 1 else print (frasealeatoria())
