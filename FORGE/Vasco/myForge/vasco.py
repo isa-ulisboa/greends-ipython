f9f388cd0c023fecddab4358406fd8c8acbe6f15
VascoFlorentino <vasco.florentino.volf@gmail.com>
2022-10-16 18:18:11 +0100

    y0.4

--- FORGE/Vasco/myForge/vasco.py
+++ FORGE/Vasco/myForge/vasco.py
@@ -1,33 +1,20 @@
 from re import X
 from typing import Sized, Text
 
-
 print ("hello")
 # 
 if 4<0:
     print ("print")
     print ("landscape urbanism")
-
 if 3>0:
     print(X)
-
-
 X=6+X*2
-
 print (X)
-
 print(type(X))
-
-
-
 a= '''It´s "very" cold'''
-
-
 def my_firstFunction():
     x = 1
     y = 2
-
-
 
 """
 
@@ -35,3 +22,19 @@ FORGE is the file name
 
 """
 ### CASTING
+
+import random
+def frasealeatoria():
+    return (print('O Vasco escolheu', var1[1],'camisa', var4[1] , 'de cor',var2[0],
+     'para emprestar ao',var3[2], '.'))
+
+var1= [1,2,3,4]
+var2= ['azul','branca','verde','castanha']
+var3= ['Antonio','Ze','Roberto','Afonso']
+var4= {"""(s)"""}
+
+random.shuffle(var3)
+random.shuffle(var2)
+random.shuffle(var1)
+
+print (frasealeatoria())
