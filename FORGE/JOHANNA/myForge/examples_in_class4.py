4fbc907c5236ed1407b7076a8b3b0fa0ef3eb2c9
Johanna Rauberger <johannarauberger@edu.ulisboa.pt>
2022-10-16 16:52:20 +0200

    assignment 5

--- FORGE/JOHANNA/myForge/examples_in_class4.py
+++ FORGE/JOHANNA/myForge/examples_in_class4.py
@@ -1,9 +1,9 @@
 # make a sample file with the first 1000
 f = open("GreenDSfile1.csv")
 lst = f.readlines()
-f.close()
+f.close() # if opening a file into an object, always close it- otherwise it will stay open.
 
-# if opening a file into an object, always close it- otherwise it will stay open.
+
 fout = open("GreenDSfile1_sample.csv", "w")
 for i in range(100):
     fout.writelines(lst[i])
