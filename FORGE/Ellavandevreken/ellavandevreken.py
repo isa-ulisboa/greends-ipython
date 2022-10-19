a7b10dc33f0440b62d5260ed5200d5925307642a
Ellavdv99 <ella@edu.ulisboa.pt>
2022-10-19 16:45:18 +0100

    temp commit

--- FORGE/Ellavandevreken/ellavandevreken.py
+++ FORGE/Ellavandevreken/ellavandevreken.py
@@ -1,3 +1,4 @@
+import net
 import myFunctions      #dit is in dezelfde folder als ellavandevreken.py
 import myForge.myOtherFunctions    #myOtherFunctions is in another folder nl in myForge dus dot erbij
                                     #bij import zoekt python in de eigen directory
@@ -24,3 +25,31 @@ from myForge import myOtherFunctions         # same als lijn hierboven
 res = myForge.myOtherFunctions.myspecialfunction()
 res = mof.myspecialfunction()      #door lijn 20
 res = myspecialfunction()          #door lijn 21 (of 22)
+
+
+#opdracht tijdens les 14.10
+f = open("GreenDSfile1.csv") 
+lst = f.readlines()
+f.close()
+
+fout = open("GreenDSfile1_sample.csv", "w")
+for i in range(100): #van 0 tot 99
+    fout.writelines(lst[i])
+fout.close()
+
+## nu hebben we een document aangemaakt met de naam GreenDSfile1_sample bestaande uit de eerste 100 lijnen van het document GreenDSfile1 (alle lijnen zitten opgeslagen in de variabele lst) 
+
+#how to delete a file: 
+import os
+os.remove("GreenDSfile1_sample.csv")
+
+#counting lines (kweenie wat dit hieronder juist is mar check zijn document in de drive: "slingshot2")
+for i in range(10):
+    fp = open("GreenDSfile1.csv",'r')
+    x = len(fp.readlines())
+    fp.close()
+    print('Total lines:',x)
+
+import netCDF4 as nc
+#deze lijn kan je nog niet uitvoeren want ik moest eerst nog pip downloaden eenmaal dat gebeurd is moet je in terminal intypen: 
+#pip install netCDF4
