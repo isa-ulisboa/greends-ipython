4f25339f43ecc230f548bfe1c12b9144f4883b3c
borneah <saraasenra@icloud.com>
2022-10-19 14:21:45 +0100

    ex01_ex02

--- FORGE/sarasenra.py
+++ FORGE/sarasenra.py
@@ -1,17 +1,4 @@
-f = open("GreenDSfile1.csv")
-lst = f.readlines()
-f.close()
+x = 2
+y = x + 4
 
-fout = open("GreenDSfile1_sample.csv","w")
-for i in range(100):
-    fout.writelines(lst[1])
-fout.close()
-
-import os
-
-os.remove("GreenDSfile1_sample.csv")
-
-fp = open("GreenDSfile1.csv", 'r')
-x = len(fp.readlines())
-print('Total lines:', x)
-fp.close()
+print [y]
