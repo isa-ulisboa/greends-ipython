d8c602d8f34d0b0238ecbeb2577261b883db55d4
Samuel Guerreiro <samuelguerreiro@edu.ulisboa.pt>
2022-11-09 11:12:46 +0000

    Correction: storing the first 500000 lines

--- FORGE/SAMUEL/myForge/filehandling.py
+++ FORGE/SAMUEL/myForge/filehandling.py
@@ -13,20 +13,23 @@ print(i)  #dá o número de linhas do ficheiro: 2.000.000
 
 #para eliminar a 1.ª linha (quando há título, p.ex.): header = next(csvreader)
 
+
 #para armazenar as primeiras 500000 linhas:
 lst = []  #criar uma lista vazia
 i=0       
 
 with open ("GreenDSfile1.csv", 'r') as file:  
     csvreader = csv.reader(file)
-    for row in csvreader:
-        while i < 500000:  #para considerar as primeiras 500.000 linhas do ficheiro
-            i=i+1
+    for row in list(csvreader)[0:500000]:
+        #while i < 500000:  #para considerar as primeiras 500.000 linhas do ficheiro, mas dá zeros e não pode
+            #i=i+1
             lst.append(row)  #para adicionar essas linhas à lista criada acima
+            print(row)
 
 with open ("GreenDSfile1_sample500k.csv", 'w') as file:  #"w" de writing
     for row in lst:
         file.write(str(row)+ ', ')  #armazena a lista no ficheiro criado
+
 
 #para armazenar as últimas 100 linhas:
 lst = []
