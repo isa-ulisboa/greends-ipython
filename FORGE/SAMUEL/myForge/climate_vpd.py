5df302f03c6a6c36cf74f5858c4102a0b41f1b32
Samuel Guerreiro <samuelguerreiro@edu.ulisboa.pt>
2022-11-09 13:26:57 +0000

    update #ass.6

--- FORGE/SAMUEL/myForge/climate_vpd.py
+++ FORGE/SAMUEL/myForge/climate_vpd.py
@@ -1,22 +1,26 @@
+#create code to open the file "Climate_hisafe_Kminevaluation_..." and store info at a list
 import csv
 import math
-#from myFunctions import calculate_vpd
+
 with open ("Climate_hisafe_knmievaluation_daily_from_01-01-1981_to_31-12-2010.csv", 'r') as ch:
     csvreader = csv.reader(ch)
 
-    header = next(csvreader)
+    header1 = next(csvreader)
     header2 = next(csvreader)
 
     lst = []
     i=0
     for row in csvreader:
         lst.append(row)
-print(lst[0])
+print(lst[0])               #se quiser ver o que existe na 1.ª linha
 
+
+#create a new list called "lst_vpd" and populate the list with result function "calculate_vpd"
 lst_vpd = []
 i=0
 
-def calculate_vpd (Tmax, Tmin, RHmean):
+#from myFunctions import calculate_vpd
+def calculate_vpd (Tmax, Tmin, RHmean):    #função aqui pq não estava a fazer o import acima
     val1 = 0.6108
     val2 = math.exp((17.27*Tmax)/(Tmax + 273.3))
     val3 = val1
@@ -28,19 +32,20 @@ def calculate_vpd (Tmax, Tmin, RHmean):
     VPD = Es - Ea
 
     return(VPD)
+
 for row in lst:
     lst_vpd.append(calculate_vpd(Tmax=float(row[3]), Tmin=float(row[4]), RHmean=float(row[5])))
 print(lst_vpd[0])
 
-
-#retirar da linha 19-30 e ativar a linha 3 (pq o commit n estava a funcioinar)
 lst_final = []
 for i in range(0,len(lst)):
     lst_final.append(lst[i])
     lst_final.append(lst_vpd[i])
 print(lst_final[0])
+
 with open ("Climate_hisafe_knmievaluation_daily_from_01-01-1981_to_31-12-2010_vpd.csv", 'w') as ch:
-    
     for row in lst_final:
-        ch.write(str(row))
-    
+        for i in range(0, len(row)):
+           ch.write(str(row[i]) + ",")    
+           print(row[i])
+
