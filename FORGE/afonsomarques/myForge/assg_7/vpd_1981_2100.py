4bafcc315fc4237776e0947a51fb796fc5ce11a2
afosnom4rques <marques.afonso@edu.ulisboa.pt>
2022-11-15 17:21:46 +0000

    assg_7_final

--- FORGE/afonsomarques/myForge/assg_7/vpd_1981_2100.py
+++ FORGE/afonsomarques/myForge/assg_7/vpd_1981_2100.py
@@ -6,7 +6,6 @@ file_2100 = 'Climate_hisafe_knmircp85_daily_from_01-01-2006_to_31-12-2100.csv'
 
 import pandas as pd
 import function as f
-import csv 
 import matplotlib.pyplot as plt
 Current = pd.read_csv(file_2010, skiprows=[0], index_col=False)
 Future = pd.read_csv(file_2100, skiprows=[0], index_col=False)
@@ -16,59 +15,23 @@ Future = pd.read_csv(file_2100, skiprows=[0], index_col=False)
 Current["VPD"] = f.vap_pres_def(Current["tasmax"],Current["tasmin"],Current["hurs"] )
 Future["VPD"] = f.vap_pres_def(Future["tasmax"],Future["tasmin"],Future["hurs"] )
 
-
-Current.to_csv(path_assg_7+'climate_data+vpd_1981-2010.csv')
-Future.to_csv(path_assg_7+'climate_data+vpd_2006-2100.csv')
-
-
+# calculate the mean by month
 mean_c_mth = Current.groupby(['Month']).mean()
-print(mean_c_mth['VPD'])
-
 mean_f_mth = Future.groupby(['Month']).mean()
-print(mean_f_mth['VPD'])
 
-mean_c_mth['VPD'].plot(label='Current vpd', color='red')
-mean_f_mth['VPD'].plot(label='Future vpd', color='green')
+# adding the vpd mean curves 
+mean_c_mth['VPD'].plot(label='VPD 1981-2010', color='red')
+mean_f_mth['VPD'].plot(label='VPD 2006-2100', color='green')
 
-
-
+plt.title('VPD Comparision')
+# adding Label to the x-axis
+plt.xlabel('Month')
+# adding Label to the y-axis
+plt.ylabel('Milibars')
+# adding legend to the curves based on the label defined previuously
+plt.legend()
+# adding a grid
+plt.grid()
+# showing the plot
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
