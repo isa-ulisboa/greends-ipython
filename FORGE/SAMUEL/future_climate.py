4a82f85712d2c66361dbefb1a41255ddfd640f93
Samuel Guerreiro <samuelguerreiro@edu.ulisboa.pt>
2022-11-22 21:44:44 +0000

    Assignment 7 (concluded)

--- FORGE/SAMUEL/future_climate.py
+++ FORGE/SAMUEL/future_climate.py
@@ -1,3 +1,5 @@
+#ASSIGNMENT 7
+
 import sys
 # (Facultative) setting path to get your myfunctions 
 sys.path.append('../FORGE/SAMUEL') # This adds your modules if your module is somewhere else not script folder or subfolders
@@ -13,5 +15,31 @@ Current["VPD"] = myFunctions.calculate_vpd_7(Current["tasmax"],Current["tasmin"]
 Future["VPD"] = myFunctions.calculate_vpd_7(Future["tasmax"],Future["tasmin"],Future["hurs"] )
 #print(Current["tasmax"])
 
-#Current.insert(10,column="VPD",value=Current["VPD"])
-#Future.insert(10,column="VPD",value=Future["VPD"])
+#Current.insert(10,column="VPD",value=myFunctions.calculate_vpd_7(Current["tasmax"],Current["tasmin"],Current["hurs"] ))
+#Future.insert(10,column="VPD",value=myFunctions.calculate_vpd_7(Future["tasmax"],Future["tasmin"],Future["hurs"] ))
+
+
+#calcular média mensal
+current_1981_2010=Current[(Current["Year"]>=1981)&(Current["Year"]<=2010)]
+future_2031_2060=Future[(Future["Year"]>=2031)&(Future["Year"]<=2060)]
+
+#calcular um novo dataset para ter a média mensal
+mean_current=current_1981_2010.groupby(["Month"]).mean()
+mean_current["VPD"]
+print(mean_current["VPD"]) #queremos imprimir a coluna VPD com a média já calculada
+
+mean_future=future_2031_2060.groupby(["Month"]).mean()
+mean_future["VPD"]
+print(mean_future["VPD"]) 
+
+#criar gráfico
+import matplotlib as mp #mat e não Mapt...
+
+mean_current["VPD"].plot(label="VPD", color="blue") #label é o nosso Y e queremos que tenha cor blue
+mp.pyplot.suptitle("VPD_current_mean") # para escrever o título
+mp.pyplot.show()
+
+mean_future["VPD"].plot(label="VPD", color="blue") #label é o nosso Y e queremos que tenha cor blue
+mp.pyplot.suptitle("VPD_future_mean") # para escrever o título
+mp.pyplot.show()
+
