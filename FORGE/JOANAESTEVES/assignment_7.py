be94c9f73b74cff26020f8faf4f367787b8ddedf
jfilipa1 <jfilipa1@edu.ulisboa.pt>
2022-11-22 16:22:52 +0000

    updating

---
+++ FORGE/JOANAESTEVES/assignment_7.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('c:/Users/joana/Desktop/GDS/INTPY/greends-ipython-1/FORGE/JOANAESTEVES')
+
+# We need pandas and myfunctions file for this exercise
+import pandas as pd
+import myFunctions
+
+Current = pd.read_csv()
+
