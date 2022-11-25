6c7a6f078b5db00fe142c517a687239edab14d1a
luisgcms <luis-m-soares@edu.ulisboa.pt>
2022-11-25 15:29:40 +0000

    Assignment 6

--- FORGE/LUISSOARES/myFunctions.py
+++ FORGE/LUISSOARES/myFunctions.py
@@ -5,3 +5,25 @@ def myFunction():
 
 def myFunction1():
     return "Hello World?"
+
+
+# Assignment nº6
+import numpy as np
+
+def calculate_vpd(Tmax, Tmin, RHmean):
+    """Function calculates daily Vapor Pressure Deficit (kPa) between the years 1981-2010, acording to FAO 56.
+
+    Args:
+        Tmax (float): Maximum recorded temperature (ºC)
+        Tmin (float): Minimum recorded temperature (ºC)
+        RHmean (float): Mean of the recorded relative humidity (%)
+
+    Returns:
+        float: Daily vapor pressure deficit values (kPa)
+    """
+
+    es = (0.6108*np.exp(17.27*Tmax/(Tmax + 237.3)) + 0.6108*np.exp(17.27*Tmin/(Tmin + 237.3))) / 2
+
+    ea = es*(RHmean/100)
+
+    return es - ea
