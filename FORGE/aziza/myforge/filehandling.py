0a2910ad8ffbf814261bf9860879472e0d5ea6f5
azizatanfous <isa127156@isa.ulisboa.pt>
2022-11-27 01:30:35 +0100

    assign5

--- FORGE/aziza/myforge/filehandling.py
+++ FORGE/aziza/myforge/filehandling.py
@@ -1,24 +1,27 @@
+import csv    
+#Open the file
 f=open("GreenDSfile1.csv")
 lst= f.readline()
-f.close
-
-f1 = open("GreenDSfile1.csv", 'r')
-x = len(f1.readlines())
-print('Total lines:', x) 
-f1.close()
-
-f2= open("GreenDSfile1sample500000.csv", "w")
+f.close()
+ 
+#Count the number of lines and Print the number of files
+fp = open("GreenDSfile1.csv", 'r')
+x = len(fp.readlines())
+print("total lines:", x)
+fp.close()
+#Create a new file named GreenDSfile1_sample500k.csv to store the first 500 000
+fp2= open("GreenDSfile1sample500k.csv", "w")
 for i in range(500000):
-    f2.writelines(lst[i])
-f2.close()
-
-f3 = open("GreenDSfile1.csv")
-lst = f3.readlines()
-last_lines=lst[-100:]
-f3.close()
-
-f4 = open("GreenDSfile1samplelast100.csv", "w")
+    fp2.writelines(lst[i])
+fp2.close()
+#Create a new file named GreenDSfile1_samplelast100.csv to store the last 100 lines
+fp3 = open("GreenDSfile1.csv")
+lst = fp3.readlines()[-100:]
+fp3.close()
+fp4 = open("GreenDSfile1_sample.csv", "w")
 for i in range(100):
-    f4.writelines(last_lines[i])
-f4.close()
-
+    fp4.writelines(lst[i])
+fp4.close()
+# Delete a file you should import os - operating system
+import os
+os.remove("GreenDSfile1_sample.csv")
