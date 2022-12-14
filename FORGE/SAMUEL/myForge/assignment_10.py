9dcf622e0fdd486fb8daf20b4b1170f22c685125
Samuel Guerreiro <samuelguerreiro@edu.ulisboa.pt>
2022-12-14 17:59:39 +0000

    ass. 10

--- FORGE/SAMUEL/myForge/assignment_10.py
+++ FORGE/SAMUEL/myForge/assignment_10.py
@@ -2,4 +2,58 @@ import netCDF4 as nc
 import numpy as np
 import json
 import sys
+import myFunctions as mf
+import ndeposition
 
+lat = sys.argv[2]
+print(lat)
+
+lon = sys.argv[4]
+
+file_name = sys.argv[6]
+
+ds = nc.Dataset(file_name)
+
+lats = ds.variables['lat'][:]
+lons = ds.variables['lon'][:]
+
+lat_index = mf.nearest_index(37.9, lats)
+lon_index = mf.nearest_index(-7.8, lons)
+
+nearest_lat = lats[lat_index]
+nearest_lon = lons[lon_index]
+
+dry_OXNdep = ds.variables['DDEP_OXN_m2Grid'][:]   #Dry deposition of oxidized nitrogen per m2 grid (pagina emep)
+dry_RDNdep = ds.variables['DDEP_RDN_m2Grid'][:]   #Dry deposition of reduced nitrogen per m2 grid (pagina emep)
+wet_OXNdep = ds.variables['WDEP_OXN'][:]          #wet deposition of oxidized nitrogen (pagina emep)
+wet_RDNdep = ds.variables['WDEP_RDN'][:]         #wet deposition of reduced nitrogen (pagina emep)
+
+d1 = dry_OXNdep[0, lat_index, lon_index]   #d de dry
+d2 = dry_RDNdep[0, lat_index, lon_index]
+w1 = wet_OXNdep[0, lat_index, lon_index]    #w de wet
+w2 = wet_RDNdep[0, lat_index, lon_index]
+
+ndeposition.
+
+a = {
+    "version": "0.1",
+    "version_notes": "Exercise for Green DataScience. Data extracted from 2019 EMEP (www.emep.int) dataset EMEP01_rv4.42_year.2019met_2019emis.nc with 0.1 degrees resolution (approx 11.1 km) - Dry and wet deposition of oxidized and reduced N (DDEP_OXN_m2Grid + DDEP_RDN_m2Grid + WDEP_OXN + WDEP_RDN) converted from mgN m-2 to kg ha-1",
+    "coordinate_requested": {
+        "lon": lat,
+        "lat": lon
+    },
+    "coordinate_retrieved": {
+        "lon": nearest_lon,
+        "lat": nearest_lat
+    },
+    "data": {
+        "total_n_deposition": {
+            "value": final_nitrogen,
+            "unit": "kg ha-1"
+        }
+    }
+}
+
+with open ("assignemtn9.json", "w") as f:
+    b=json.dumps(a)
+    json.dump(b,f)
--- FORGE/SAMUEL/myForge/ndeposition.py
+++ FORGE/SAMUEL/myForge/ndeposition.py
@@ -3,40 +3,44 @@
 import netCDF4 as nc 
 import numpy as np
 import json
-fn = 'EMEP01_rv4.42_year.2019met_2019emis.nc'  # update this path to your own file
+import myFunctions as mf
 
-ds = nc.Dataset(fn)
+fn = 'EMEP01_rv4.42_year.2019met_2019emis.nc'  # update this path to your own file
 
 #imput 
 in_lat = 37.9
 in_lon = -7.8
 
+def calculate_deposition (fn, in_lat, in_lon): #assignment 10
+    ds = nc.Dataset(fn)
+
 #list of the lat, lon, coordinates and deposition
-lats = ds.variables['lat'][:]
-lons = ds.variables['lon'][:]
-dry_OXNdep = ds.variables['DDEP_OXN_m2Grid'][:]   #Dry deposition of oxidized nitrogen per m2 grid (pagina emep)
-dry_RDNdep = ds.variables['DDEP_RDN_m2Grid'][:]   #Dry deposition of reduced nitrogen per m2 grid (pagina emep)
-wet_OXNdep = ds.variables['WDEP_OXN'][:]          #wet deposition of oxidized nitrogen (pagina emep)
-wet_RDNdep = ds.variables['WDEP_RDN'][:]         #wet deposition of reduced nitrogen (pagina emep)
+    lats = ds.variables['lat'][:]
+    lons = ds.variables['lon'][:]
+    dry_OXNdep = ds.variables['DDEP_OXN_m2Grid'][:]   #Dry deposition of oxidized nitrogen per m2 grid (pagina emep)
+    dry_RDNdep = ds.variables['DDEP_RDN_m2Grid'][:]   #Dry deposition of reduced nitrogen per m2 grid (pagina emep)
+    wet_OXNdep = ds.variables['WDEP_OXN'][:]          #wet deposition of oxidized nitrogen (pagina emep)
+    wet_RDNdep = ds.variables['WDEP_RDN'][:]         #wet deposition of reduced nitrogen (pagina emep)
 
-import myFunctions as mf
 
-lat_index = mf.nearest_index(37.9, lats)
-lon_index = mf.nearest_index(-7.8, lons)
+    lat_index = mf.nearest_index(37.9, lats)
+    lon_index = mf.nearest_index(-7.8, lons)
 
-nearest_lat = lats[lat_index]
-nearest_lon = lons[lon_index]
+    nearest_lat = lats[lat_index]
+    nearest_lon = lons[lon_index]
 
-d1 = dry_OXNdep[0, lat_index, lon_index]   #d de dry
-d2 = dry_RDNdep[0, lat_index, lon_index]
-w1 = wet_OXNdep[0, lat_index, lon_index]    #w de wet
-w2 = wet_RDNdep[0, lat_index, lon_index]
+    d1 = dry_OXNdep[0, lat_index, lon_index]   #d de dry
+    d2 = dry_RDNdep[0, lat_index, lon_index]
+    w1 = wet_OXNdep[0, lat_index, lon_index]    #w de wet
+    w2 = wet_RDNdep[0, lat_index, lon_index]
 
-nitrogen = d1+d2+w1+w2
-print (nitrogen)
+    nitrogen = d1+d2+w1+w2
+    print (nitrogen)
 
-#conversão unidades
-final_nitrogen = nitrogen/100
+    #conversão unidades
+    final_nitrogen = nitrogen/100
+
+    return (final_nitrogen)
 
 a = {
     "version": "0.1",
