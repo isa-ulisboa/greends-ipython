ee4a0e473a88687987f88f3f7229aed8985ef718
joaopalma <joaopalma@isa.ulisboa.pt>
2022-12-19 13:18:43 +0000

    Group Assignment

---
+++ ASSIGNMENTS/GroupAssignment/group1.py
@@ -0,0 +1,23 @@
+# Group1 is responsible to provide a dictionary with weather forecast for the endpoint
+# an example of the output is provided in material.Output1Group1
+def getHourlyWeatherForescast(lat,lon):
+    """Accesses open-meteo.com and retrieves, for location Lat, Lon the current weather and the hourly forecast for :
+       temperature_2m
+       relativehumidity_2m
+       precipitation
+       soil_moisture_3_9cm
+       soil_moisture_9_27cm
+       vapor_pressure_deficit
+       et0_fao_evapotranspiration   
+
+    Args:
+        lat (float): Latitude in deimal degrees
+        lon (float): Longitude in decimal degrees
+
+    Returns:
+        dictionary: A dictionary with retrieved data from open-meteo
+    """
+    #Output1 is to be replaced by your weather forecast retrieval from open-meteo API
+    Output1={}
+    return Output1
+
