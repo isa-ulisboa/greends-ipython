3c0df5518eaaa86a374870afd67d5d0e7f184815
Manuel Campagnolo <mlc@isa.ulisboa.pt>
2024-09-13 10:03:33 +0100

    Delete ETC directory

--- ETC/args_kwargs_basic_example.py
+++
@@ -1,6 +0,0 @@
-def pack(*args, **kwargs):
-    print('Positional (*)',args)
-    print('Named (**)',kwargs)
-    return args,kwargs
-
-print(pack(1,2,10, num_years=10,rate=0.03))
--- ETC/unpacking.py
+++
@@ -1,3 +0,0 @@
-x=[1,2,3,4,5,6,7,8,9]
-a,*b,c=x # b is a list
-print(a,b,c)
